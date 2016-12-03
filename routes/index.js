28631a03e8a04b9b3d50c13d33ab1c1c0085d579
Jason Stallings <jason@stallin.gs>
2016-12-02 22:03:29 -0600

    Basic app.

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // Check to see if a Wikipedia article exists for "Hitler".
+  request('https://en.wikipedia.org/w/api.php?action=query&titles=Hitler&prop=info&format=json', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var json = JSON.parse(body);
+      var hasTimeTravelBeenInvented = Object.keys(json.query.pages).length > 0 ? 'NO' : 'YES';
+      res.render('index', { hasTimeTravelBeenInvented });
+    }
+  })
+});
+
+module.exports = router;
